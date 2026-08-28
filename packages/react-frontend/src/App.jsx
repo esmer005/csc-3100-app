499dc71fe39b77b0dea23e62b81029fa78456a77
Esmeralda Flores <esmerflores17@gmail.com>
2026-08-28 16:01:35 -0700

    final IE0

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>This Is Where It All Begins.</h1>
+          <h1>Esmerald Flores</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
