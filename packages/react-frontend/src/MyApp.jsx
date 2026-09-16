7aaa72a9e43318df96afc12b38d4478b75aecada
Esmeralda Flores <esmerflores17@gmail.com>
2026-09-16 16:36:36 -0700

    Submit button and POST

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -13,8 +13,12 @@ function MyApp() {
         setCharacters(updated);
     }
 
-    function updateList(person){
-    setCharacters([...characters, person]);
+    function updateList(person) {
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+            console.log(error);
+        });
     }
 
     function fetchUsers() {
@@ -30,6 +34,18 @@ function MyApp() {
             console.log(error);
         });
     }, []);
+
+    function postUser(person) {
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
+
+    return promise;
+}
 
     return (
         <div className="container">
